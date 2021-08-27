350146a24e7123ce7477b72143c0a6e0949fc47b
Lloyd Worth <lloydrww@gmail.com>
2021-08-27 18:43:33 +0000

    tinkering - no changes

--- express_server.js
+++ express_server.js
@@ -4,7 +4,9 @@ const app = express();
 
 const PORT = 8080; // default port 8080
 
-const urlDatabase = {
+
+// this is a JSON database of urls...proto shortener.
+const urlDatabase = {   
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -21,3 +23,16 @@ app.listen(PORT, () => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>world</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+ });
+ 
+ app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+ });
