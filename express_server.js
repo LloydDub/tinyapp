ce538a63adc48766ed0018ab7ec06d9b800ad1e6
Lloyd Worth <lloydrww@gmail.com>
2021-08-31 20:26:37 +0000

    added cookie based login and logout routes

--- express_server.js
+++ express_server.js
@@ -1,12 +1,16 @@
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
 const PORT = 8080; // default port 8080
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.set('view engine', 'ejs');
 
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 // app.post('/urls', (req, res) => {
@@ -14,8 +18,14 @@ app.use(bodyParser.urlencoded({extended: true}));
 //   res.send('Ok');         // Respond with 'Ok' (we will replace this)
 // });
 
+app.post("/login", (req,res) => {
+  res.cookie('username', req.body.username);
+  res.redirect("/urls");
+});
+
+
 app.post('/urls', (req, res) => {
-  const shortURL = generateRandomString(); // if long url exsists 
+  const shortURL = generateRandomString(); // if long url exsists
   urlDatabase[shortURL] = req.body.longURL;
   
   res.redirect(`urls/${shortURL}`);
@@ -29,11 +39,11 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 });
 
 app.post('/urls/:shortURL', (req, res) => {
-  console.log(req.params)
-  console.log(req.params.shortURL)
+  console.log(req.params);
+  console.log(req.params.shortURL);
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect('/urls');
-})
+});
 
 
 
@@ -42,13 +52,21 @@ app.post('/urls/:shortURL/delete', (req, res) => {
   res.redirect('/urls');
 
 });
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
+
+
 // app.get('/urls/:shortUrl', (req, res) => {
 //   res.redirect(`urls/${req.params.shortURL}`);
 
 // })
 
 // this is a JSON database of urls...proto shortener.
-const urlDatabase = {   
+const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
@@ -59,7 +77,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 // reguest handler for urldatabase
@@ -83,30 +101,37 @@ app.get('/fetch', (req, res) => {
 
 //passes the URL data to temmplate3
 app.get('/urls', (req, res) => {
-   const templateVars = { urls: urlDatabase };
-   res.render('urls_index', templateVars);
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase 
+  };
+  res.render('urls_index', templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
+  let templateVars = { username: req.cookies["username"] };
+  res.render("urls_new", templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }; 
+
+  const templateVars = { 
+    username: req.cookies["username"],
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL] 
+  };
   res.render('urls_show', templateVars);
 });
 
 app.get('/u/:shortURL', (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]
+  const longURL = urlDatabase[req.params.shortURL];
   if (!urlDatabase[req.params.shortURL]) {
     return res.send('Error, please check your shortened URL');
- }
-res.redirect(longURL);
+  }
+  res.redirect(longURL);
 });
 
 function generateRandomString() {
-  const result = (Math.random() + 1).toString(36).substring(6)
+  const result = (Math.random() + 1).toString(36).substring(6);
   return result;
-};
-
+}
