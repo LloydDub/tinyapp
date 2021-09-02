787700ac5db63abe1434fe8ce938dbe64c1c9155
Lloyd Worth <lloydrww@gmail.com>
2021-09-02 04:45:50 +0000

    added the registration handler

--- express_server.js
+++ express_server.js
@@ -1,29 +1,58 @@
 const express = require('express');
-
 const app = express();
-
 const PORT = 8080; // default port 8080
-
 const cookieParser = require('cookie-parser');
 app.use(cookieParser());
-
 app.set('view engine', 'ejs');
-
 const bodyParser = require('body-parser');
-
 app.use(bodyParser.urlencoded({extended: true}));
 
-// app.post('/urls', (req, res) => {
-//   console.log(req.body);  // Log the POST request body to the console
-//   res.send('Ok');         // Respond with 'Ok' (we will replace this)
-// });
+// GLOBAL STUFF \\
 
+//Random string Generator
+function generateRandomString() {
+  const result = (Math.random() + 1).toString(36).substring(6);
+  return result;
+}
+
+//Global user object
+const users = { 
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+ "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+// this is a JSON database of urls...proto shortener.
+const urlDatabase = {
+  'b2xVn2': 'http://www.lighthouselabs.ca',
+  '9sm5xK': 'http://www.google.com'
+};
+
+// 
+const addUser = (email, password) => {
+  const id = generateRandomString()
+  users[id] = {
+    id,
+    email,
+    password
+  };
+  return id;
+};
+
+// Makes a username cookie to store user data
 app.post("/login", (req,res) => {
   res.cookie('username', req.body.username);
   res.redirect("/urls");
 });
 
- 
+ // POST for generating smoll url w\ genrandom string
 app.post('/urls', (req, res) => {
   const shortURL = generateRandomString(); // if long url exsists
   urlDatabase[shortURL] = req.body.longURL;
@@ -32,6 +61,7 @@ app.post('/urls', (req, res) => {
   
 });
 
+// POST for handling delete function
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect('/urls');
@@ -39,8 +69,7 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 });
 
 app.post('/urls/:shortURL', (req, res) => {
-  console.log(req.params);
-  console.log(req.params.shortURL);
+  
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect('/urls');
 });
@@ -53,28 +82,28 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 
 });
 
+//POST for logout button
 app.post("/logout", (req, res) => {
   res.clearCookie('username');
   res.redirect("/urls");
 });
 
 
-
-// app.get('/urls/:shortUrl', (req, res) => {
-//   res.redirect(`urls/${req.params.shortURL}`);
-
-// })
-
-// this is a JSON database of urls...proto shortener.
-const urlDatabase = {
-  'b2xVn2': 'http://www.lighthouselabs.ca',
-  '9sm5xK': 'http://www.google.com'
-};
+// simple username login using cookies
+app.get('/register', (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase 
+  };
+  res.render("urls_register", templateVars);
+});
 
 // this registers the a handler on the root path of '/'
 app.get('/', (req, res) => {
-  res.send('Hello!');
+  res.redirect("/urls");
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
@@ -85,19 +114,8 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-//
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>world</b></body></html>\n');
-});
 
-app.get('/set', (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
-});
- 
-app.get('/fetch', (req, res) => {
-  res.send(`a = ${a}`);
-});
+
 
 //passes the URL data to temmplate3
 app.get('/urls', (req, res) => {
@@ -108,6 +126,7 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
+//template for new urls page w/username banner
 app.get('/urls/new', (req, res) => {
   let templateVars = { username: req.cookies["username"] };
   res.render("urls_new", templateVars);
@@ -131,7 +150,14 @@ app.get('/u/:shortURL', (req, res) => {
   res.redirect(longURL);
 });
 
-function generateRandomString() {
-  const result = (Math.random() + 1).toString(36).substring(6);
-  return result;
-}
+app.post('/register', (req, res) => {
+  const {email, password } = req.body;
+ 
+  const user_id = addUser(email, password);
+  res.cookie('user_id', user_id);
+  
+  res.redirect('/urls');
+});
+
+
+
